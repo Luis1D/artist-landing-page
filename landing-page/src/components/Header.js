420dc54619423f500ed0ca8c8b6c53f149bc8198
Luis De Anda <luisdeanda916@outlook.com>
2020-02-17 16:24:15 -0800

    sass added & header component created

---
+++ landing-page/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <nav>
+                <ul>
+                    <li>Spotify</li>
+                    <li>SoundCloud</li>
+                    <h1 className="Logo">Betz Beats</h1>
+                    <li>TikTok</li>
+                    <li>Instagram</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
